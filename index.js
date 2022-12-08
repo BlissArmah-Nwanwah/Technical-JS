702d3cc135921bb12fbe252d3032a7d489ee1f17
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-08 07:59:49 +0000

    codecademy skill path with java

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-kmklmg
+const Isnumber = [];
+
+const GiveNumbers = (num) => Isnumber.push(num);
+const AvailabelNumber = (num) => Isnumber.includes(num)
+
+console.log(GiveNumbers(5));
+console.log(GiveNumbers(8));
+console.log(GiveNumbers(1));
+console.log(Isnumber)
+console.log(AvailabelNumber(2
+));
